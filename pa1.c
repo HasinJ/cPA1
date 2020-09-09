e4141d7191f952568b285a51a0e3bf952a5c6dbc
HasinJ <hasinchoudhuryj@gmail.com>
2020-09-09 16:23:06 -0400

    no more than one file, and file doesnt exist

--- pa1.c
+++ pa1.c
@@ -2,6 +2,18 @@
 #include<stdlib.h>
 
 int main(int argc, char* argv[argc +1]) {
+  if (argc>2 || argc==1) {
+    printf("Please enter 1 file name.\n");
+    return EXIT_SUCCESS;
+  }
+
+  FILE *f = fopen(argv[1],"r");
+  if (f==0) {
+    printf("File doesn't exist.\n");
+    return EXIT_SUCCESS;
+  }
+
+
   for (size_t i = 1; i < argc; i++) {
     printf("%s\n", argv[i]);
   }
