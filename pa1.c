95245ee7b6951dae29cfea48b6f42cf01b373ed3
HasinJ <hasinchoudhuryj@gmail.com>
2020-09-14 17:46:47 -0400

    needs test in ilab

--- pa1.c
+++ pa1.c
@@ -7,14 +7,43 @@ int main(int argc, char* argv[argc +1]) {
     return EXIT_SUCCESS;
   }
 
-  FILE *f = fopen(argv[1],"r");
-  if (f==0) {
-    printf("File doesn't exist.\n");
+  FILE *f;
+  f = fopen(argv[1],"r");
+  if (f==0 || f==NULL) {
+    printf("File error.\n");
     return EXIT_SUCCESS;
   }
 
+  int i=0;
+  char line = fgetc(f);
+  while (line!=EOF) {
+    printf("%c", line);
+    line = fgetc(f);
+    i++;
+  }
+  printf("\n%d\n", n);
 
+  printf("other way:\n");
+  int x=0;
+  int number;
+  while (fscanf(f,"%[^\n]\n",number) != EOF ) {
+    printf("%d\n", number);
+    x++;
+  }
+
+/*
+  int *numbers = (int*) malloc(n * sizeof(int));
+  for (size_t i = 0; i < n; ++i) {
+    printf("%c", line);
+    //fscanf(f, "%d", numbers+i);
+    line = fgetc(f);
+  }
+  fclose(f);
+  free(numbers);
+*/
   for (size_t i = 1; i < argc; i++) {
     printf("%s\n", argv[i]);
   }
+
+
 }
