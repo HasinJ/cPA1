720aac35045cca6f94dce455ab05be899747bc35
HasinJ <hasinchoudhuryj@gmail.com>
2020-09-22 18:07:29 -0400

    working with personal tests

--- fourth/fourth.c
+++ fourth/fourth.c
@@ -15,11 +15,30 @@ void read(int** matrix, int dimsions){
     }
     printf("\n");
   }
-  printf("\n");
+  //printf("\n");
 }
 
+void RowCol(int** matrix,int* row, int* col,int dimsions){
+  int temprow = *row;
+  int tempcol = *col;
+  temprow-=1;
+  tempcol+=1;
+  if(temprow<0) temprow=dimsions-1; //go to the last row
+  if(temprow>dimsions-1) temprow=0; //go to first row
 
-void Matrix(FILE* f,int dimsions) {
+  if(tempcol<0) tempcol=dimsions-1; //go to last column
+  if(tempcol>dimsions-1) tempcol=0; //go to the first column
+
+  if (matrix[temprow][tempcol]!=0) {
+    temprow=*row+1;
+    tempcol=*col;
+    if(temprow>dimsions-1) row=0; //go to first row
+  }
+  *row=temprow;
+  *col=tempcol;
+}
+
+void Matrix(int dimsions) {
   int** matrix=calloc(dimsions,sizeof(int*));//rows
   for (int i=0; i<dimsions; i++){
     matrix[i]=calloc(dimsions,sizeof(int*));//columns
@@ -27,54 +46,29 @@ void Matrix(FILE* f,int dimsions) {
 
   int count=1,row=0,col=(dimsions-1)/2;
   int* current = &matrix[row][col]; //initial row & column
-  while (*current!=dimsions*dimsions){
-    if(*current!=0){
-      row+=1; //going backwards
-      col-=1; //going backwards
-      row+=1;
-      printf("count=%d row=%d col=%d\n", count,row,col);
-      current=&matrix[row][col];
-      *current=count++;
-      row-=1;
-      col+=1;
-      if(row<0) row=dimsions-1; //go to the last row if upper element is out of reach
-      if(col>2) col=0; //go to the first column if righter element is out of reach
-      current=&matrix[row][col];
-      read(matrix,dimsions);
-      continue;
-    }
+
+  while (count<dimsions*dimsions){
+    current=&matrix[row][col]; //selects
     printf("count=%d row=%d col=%d\n", count,row,col);
-    *current=count++;
-    row-=1;
-    col+=1;
-    if(row<0) row=dimsions-1; //go to the last row if upper element is out of reach
-    if(col>2) col=0; //go to the first column if righter element is out of reach
-    current=&matrix[row][col];
+    *current=count++;//fills
     read(matrix,dimsions);
+    RowCol(matrix,&row,&col,dimsions); //changes
+    printf("NEXT count=%d row=%d col=%d\n\n", count,row,col);
   }
 
+  matrix[row][col]=count; //last
+  printf("finish:\n");
+  read(matrix,dimsions);
   freeEverything(matrix,dimsions);
 }
 
 int main(int argc, char *argv[argc+1]) {
-  if (argc>2 || argc==1) {
-    printf("Please enter 1 file name.\n");
-    return EXIT_SUCCESS;
-  }
 
-  FILE *f;
-  f = fopen(argv[1],"r");
-  if (f==0) {
-    printf("error.\n");
-    return EXIT_SUCCESS;
-  }
-
-  int x;
-  fscanf(f,"%d",&x);
-  printf("\ndimensions=%dx%d\n", x,x);
+  int x=atoi(argv[1]);
+  //printf("\ndimensions=%dx%d\n", x,x);
   //checks for even/odd
-  if ((x/2)==((x-1)/2)) Matrix(f,x); //odd
-  else printf("error.\n");
+  if ((x/2)==((x-1)/2)) Matrix(x); //odd
+  else printf("error.\n"); //even
 
 
 }
