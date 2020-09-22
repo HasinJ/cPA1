b35e68efd02858d2f1ee2fc66ba70a0a625424b6
HasinJ <hasinchoudhuryj@gmail.com>
2020-09-22 13:15:15 -0400

    before values into matrix

--- fourth/fourth.c
+++ fourth/fourth.c
@@ -1,20 +1,43 @@
+#include<stdlib.h>
+#include<stdio.h>
 
-int** read(FILE* f,int dimsions) {
-  fscanf(f,"%d",dimsions);
-  int** matrix=calloc(dimsions,size(int*));//rows
-  for (int i=0; i<dimsions; i++){
-    matrix[i]=calloc(dimsions,size(int*));//columns
-  }
+void freeEverything(int** matrix, int dimsions){
   for (size_t i = 0; i < dimsions; i++) {
-    for (size_t j = 0; j < dimsions; j++) {
-      &matrix[i,j]
-    }
+    free(matrix[i]);
   }
-
-
+  free(matrix);
 }
 
-int main(int argc, char *argv[]) {
+void read(int** matrix, int dimsions){
+  for (size_t i = 0; i < dimsions; i++) {
+    for (size_t j = 0; j < dimsions; j++) {
+      printf("%d ", matrix[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
+
+
+void Matrix(FILE* f,int dimsions) {
+  int** matrix=calloc(dimsions,sizeof(int*));//rows
+  for (int i=0; i<dimsions; i++){
+    matrix[i]=calloc(dimsions,sizeof(int*));//columns
+  }
+
+  int current = matrix[1][(dimsions/2)+1],count=0;
+  while (current!=5){
+    count++;
+    printf("%d\n", count);
+    matrix[2][2]=5;
+    current=matrix[2][2];
+  }
+
+  read(matrix,dimsions);
+  freeEverything(matrix,dimsions);
+}
+
+int main(int argc, char *argv[argc+1]) {
   if (argc>2 || argc==1) {
     printf("Please enter 1 file name.\n");
     return EXIT_SUCCESS;
@@ -25,13 +48,14 @@ int main(int argc, char *argv[]) {
   if (f==0) {
     printf("error.\n");
     return EXIT_SUCCESS;
-
-  int x=atoi(argv[2]);
-  //checks for even/odd
-  if ((x/2)!=((x-1)/2)) read(f,x);
-  else printf("error.\n");
-  return EXIT_SUCCESS;
-
   }
+
+  int x;
+  fscanf(f,"%d",&x);
+  printf("\ndimensions=%dx%d\n", x,x);
+  //checks for even/odd
+  if ((x/2)==((x-1)/2)) Matrix(f,x); //odd
+  else printf("error.\n");
+
 
 }
