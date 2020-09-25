e79c2718eb60ee23fc1f980685d2d133ce2b40e7
HasinJ <hasinchoudhuryj@gmail.com>
2020-09-25 12:08:06 -0400

    leaksanitizer with linked list

--- fifth/fifth.c
+++ fifth/fifth.c
@@ -2,66 +2,44 @@
 #include<stdio.h>
 #include<assert.h>
 
-void freeEverything(int** matrix,int dimsions){
-  for (size_t i = 0; i < dimsions; i++) {
-    free(matrix[i]);
-  }
-  free(matrix);
-}
+struct Element{
+  int num;
+  struct Element* next;
+};
 
-void read(int **matrix, int dimsions){
-  for (size_t i = 0; i < dimsions; i++) {
-    for (size_t j = 0; j < dimsions; j++) {
-      printf("%d \t",matrix[i][j]);
-    }
-    printf("\n");
-  }
-}
-
-void getAnswer(int** matrix, int dimsions, int* answer){
-  if (dimsions==1){
-    *answer = matrix[0][0];
-    return;
-  } else{
-    if (dimsions==2){
-      int a=matrix[0][0],b=matrix[0][1],c=matrix[1][0],d=matrix[1][1];
-      *answer = (a*d)-(b*c);
-      return;
-    }else{
-      if (dimsions>2) {
-        //delete the rows and columns we dont want for now
+struct Element* first=0;
 
 
-        return;
-      }
-    }
-  }
 
-
-}
-
-void fill(FILE* f, int** matrix,int dimsions){
-  int x;
-  for (size_t i = 0; i < dimsions; i++) {
-    for (size_t j = 0; j < dimsions; j++) {
-      fscanf(f,"%d\t",&x);
-      matrix[i][j]=x;
-    }
-  }
-  int answer;
-  getAnswer(matrix,dimsions,&answer);
-  printf("answer=%d\n",answer);
+void freeEverything(struct Element* x){
+  if (x->next==0) return;
+  struct Element* temp = x;
+  x=x->next;
+  free(temp);
+  freeEverything(x);
+  return;
 }
 
 void Matrix(FILE* f,int dimsions){
-  int **matrix=calloc(dimsions,sizeof(int*));
+
+  struct Element* current=0;
   for (size_t i = 0; i < dimsions; i++) {
-    matrix[i]=calloc(dimsions,sizeof(int*));
+    current = calloc(1,sizeof(struct Element));
+    for (size_t j = 0; j < dimsions; j++) {
+      fscanf(f,"%d\t",&current->num);
+      //current->row = i;
+      //current->col = j;
+      current->next = 0;
+      if(first==0) first=current;
+      printf("current:%d\n", current->num);
+      current=current->next;
+    }
   }
-  fill(f,matrix,dimsions);
-  read(matrix,dimsions);
-  freeEverything(matrix,dimsions);
+
+
 }
+
+
 
 int main(int argc, char const *argv[argc+1]) {
   if (argc>2 || argc==1) {
@@ -85,5 +63,6 @@ int main(int argc, char const *argv[argc+1]) {
 
   Matrix(f,dimsions);
 
+  printf("\n");
   return EXIT_SUCCESS;
 }
